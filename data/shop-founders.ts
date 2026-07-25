3d8f991d6c1c9bc11b9a0d0718bba185cf3c806f
sigourneycantelo <sigourney@beauticate.com>
2026-07-25 14:25:43 +1000

    feat: add founders panel data with 19 female founders

---
+++ data/shop-founders.ts
@@ -0,0 +1,29 @@
+export type ShopFounder = {
+  name: string
+  brand: string
+  image: string
+  /** Link to interview article, vodcast, or brand shop page */
+  href: string
+}
+
+export const SHOP_FOUNDERS: ShopFounder[] = [
+  { name: 'Elise Pioch', brand: 'Maison Balzac', image: '/images/founders/elise_pioch_maison_balzac.jpg', href: '/shop/brands/maison-balzac-1' },
+  { name: 'Almira Armstrong', brand: 'Lumira', image: '/images/founders/almira_armstrong_lumira.jpg', href: '/shop/brands/lumira-1' },
+  { name: 'Jessica Sepel', brand: 'JSHealth Vitamins', image: '/images/founders/jess_sepel_js_health.jpg', href: '/shop/brands/jshealth-vitamins-aus' },
+  { name: 'Mukti', brand: 'Mukti Organics', image: '/images/founders/mukti_mukti.jpg', href: '/shop/brands/mukti-organics-1' },
+  { name: 'Kate Guaran & Lisa Walker', brand: 'Eir Women', image: '/images/founders/kate_guaran_lisa_walker_eir_women.jpg', href: '/shop/brands/eir-women' },
+  { name: 'Dr Leanne Girgis', brand: 'Innour', image: '/images/founders/leanne_girgis_innour.jpg', href: '/shop/brands/innour' },
+  { name: "Nicole Gazal O'Neill", brand: 'Bon Wellness', image: '/images/founders/nicole_gazal_oneill_bon_wellness.jpg', href: '/shop/brands/bon-patch' },
+  { name: 'Elly Seymour', brand: 'Saint Louve', image: '/images/founders/elly_seymour_saint_louve.jpg', href: '/shop/brands/saintlouve-1' },
+  { name: 'Tarj Mavi', brand: 'Lamav', image: '/images/founders/tarj_mavi_la_mav.jpg', href: '/shop/brands/lamav' },
+  { name: 'Farida Irani', brand: 'Subtle Energies', image: '/images/founders/farida_irani_subtle_energies.jpg', href: '/shop/brands/subtle-energies' },
+  { name: 'Lucy Bradshaw', brand: 'Tulita Parfum', image: '/images/founders/lucy_bradshaw_tulita.jpg', href: '/shop/brands/tulita-parfum' },
+  { name: 'Katrina Cochrane', brand: 'Archer Farrar', image: '/images/founders/katrina_cochrane_archer_farrar.jpg', href: '/shop/brands/archer-farrar-perfume-atelier' },
+  { name: 'Chiquita Searle', brand: 'Chiquita', image: '/images/founders/chiquita_searle_chiquita.jpg', href: '/shop/brands/chiquita' },
+  { name: 'Wanda Szychowska', brand: 'Estetika', image: '/images/founders/wanda_szychowska_estetika,.jpg', href: '/shop/brands/estetika-1' },
+  { name: 'K Orford', brand: 'OiTO Haircare', image: '/images/founders/k_orford_oito.jpg', href: '/shop/brands/oito-haircare' },
+  { name: 'Michelle Doub', brand: 'Lash Armour', image: '/images/founders/michelle_doub_lash_armour.jpg', href: '/shop/brands/lash-armour-1' },
+  { name: 'Annabelle', brand: 'Buj', image: '/images/founders/annabelle_buj.jpg', href: '/shop/brands/buj' },
+  { name: 'Jayde Houndalas', brand: 'Sontse.', image: '/images/founders/jayde_houndalas_sontse.jpg', href: '/shop/brands/sontse-1' },
+  { name: 'Bonnie Gillies', brand: 'Basics by B', image: '/images/founders/bonnie_gillies_basics_by_b.jpg', href: '/shop/brands/basics-by-b' },
+]
